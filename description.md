ecc94a88e01827a0262eed1d8f95338b8c82f56a
Andre Verissimo <afsverissimo@gmail.com>
2019-04-05 13:31:40 +0100

    initial commit

---
+++ description.md
@@ -0,0 +1,22 @@
+# Main title
+
+This is a simple example for a markdown description with a [link](http://google.com) and **some** *formating*
+
+## Second title
+
+A list is possible
+
+* with prefix \*
+* new item
+
+## Another title
+
+An ordered list is also possible!
+
+1. first
+1. second
+1. and third.. so on
+
+### Third heading
+
+Some heading
---
+++ index.js
@@ -0,0 +1,22 @@
+// If you use require (Node etc), require as first the module and then create the instance
+const Remarkable = require('remarkable');
+// If you're in the browser, the Remarkable class is already available in the window
+const md = new Remarkable();
+// Outputs: <h1>Remarkable rulezz!</h1>
+
+const fs = require('fs');
+const path = require('path');
+
+const data = md.render(fs.readFileSync(path.join(__dirname, 'description.md'), {encoding: 'utf-8'}));
+const template = fs.readFileSync(path.join(__dirname, 'template.html'), {encoding: 'utf-8'});
+
+const cheerio = require('cheerio');
+const $ = cheerio.load(template.toString());
+$('#description').html(data);
+
+const html = require("html");
+const prettyData = html.prettyPrint($.html(), {indent_size: 2});
+
+fs.writeFileSync(path.join(__dirname, 'index.html'), prettyData, {encoding: 'utf-8'});
+console.log("Written to index.html");
+
